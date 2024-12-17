955d6f76a81eaaf7dc3df031250bb89569a12b75
EdwardBejjani <edwardbejjani@gmail.com>
2024-12-17 15:46:12 +0200

    footer and resize images

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,61 @@
+<footer>
+    <div class="d-flex mt-5">
+        <div class="col-3">
+            <img src="{{ asset('assets/images/footer.png') }}" alt="" class="img-fluid img-footer">
+        </div>
+        <div class="col-2 align-content-center">
+            <img src="{{ asset('assets/images/logo.png') }}" alt="" class="img-fluid img-footer rounded-white ms-4">
+        </div>
+        <div class="col-7 text-shadow">
+            <div class="ms-5 mt-5">
+                <h3 class="text-white fw-bold text-center">Get in Touch</h3>
+                <div class="row ms-0 me-0">
+                    <div class="col-6">
+                        <ul class="list-unstyled text-center">
+                            <li style=" font-size: 2rem;"><i class="fa-solid fa-location-dot text-white"></i></li>
+                            <li class="text-white">Somewhere for example</li>
+                            <li class="text-white">Lebanon</li>
+                        </ul>
+                    </div>
+                    <div class="col-6">
+                        <ul class="list-unstyled text-center">
+                            <li style=" font-size: 2rem;"><i class="fa fa-clock text-white"></i>
+                            </li>
+                            <li class="text-white">Monday - Friday</li>
+                            <li class="text-white">08:00 - 20:00</li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="row ms-0 me-0">
+                    <div class="col-6">
+                        <ul class="list-unstyled text-center">
+                            <li style=" font-size: 2rem;"><i class="fa fa-phone text-white"></i></li>
+                            <li class="text-white">+961 70 123 456</li>
+                        </ul>
+                    </div>
+                    <div class="col-6">
+                        <ul class="list-unstyled text-center">
+                            <li style=" font-size: 2rem;"><i class="fa fa-envelope text-white"></i></li>
+                            <li class="text-white">email@example.com</li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="row ms-0 me-0">
+                    <div class="col-6">
+                        <ul class="list-unstyled text-center">
+                            <li style=" font-size: 2rem;"><i class="fa-brands fa-instagram text-white"></i></li>
+                            <li class="text-white">Instagram</li>
+                        </ul>
+                    </div>
+
+                    <div class="col-6">
+                        <ul class="list-unstyled text-center">
+                            <li style=" font-size: 2rem;"><i class="fa-brands fa-facebook text-white"></i></li>
+                            <li class="text-white">Facebook</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,6 +9,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="{{ asset('assets/css/frontend.css') }}">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" rel="stylesheet">
     <title>@yield('title') - Calories</title>
 </head>
 
