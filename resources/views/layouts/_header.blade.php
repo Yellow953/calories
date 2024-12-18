08dac4c7f7b314a8bc196a8b5ed523e40f7c47a7
EdwardBejjani <edwardbejjani@gmail.com>
2024-12-18 13:08:10 +0200

    responsive header

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -3,11 +3,31 @@
         <a class="navbar-brand" href="{{Route('home')}}">
             <img src="{{ asset('assets/images/logo.png') }}" alt="Calories Logo" class="logo" />
         </a>
+        <ul class="navbar-nav ms-auto desktop-hidden">
+            <li class="nav-item">
+                <a class="nav-link" href="#" data-bs-toggle="modal" data-bs-target="#searchModal">
+                    <i class="fa fa-search"></i>
+                </a>
+            </li>
+
+            <li class="nav-item">
+                <a class="nav-link" href="#" data-bs-toggle="modal" data-bs-target="#profileModal">
+                    <i class="fa-solid fa-user"></i>
+                </a>
+            </li>
+
+            <li class="nav-item">
+                <a class="nav-link" data-bs-toggle="offcanvas" href="#offcanvasCart" role="button"
+                    aria-controls="offcanvasCart">
+                    <i class="fa-solid fa-cart-shopping"></i>
+                </a>
+            </li>
+        </ul>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav ms-auto center-menu">
+            <ul class="navbar-nav ms-auto center-menu text-center">
                 <li class="nav-item">
                     <a class="nav-link {{Route::is('home') ? 'active': ''}}" href="{{Route('home')}}">HOME</a>
                 </li>
@@ -21,7 +41,7 @@
                     <a class="nav-link {{Route::is('contact') ? 'active': ''}}" href="{{Route('contact')}}">CONTACT</a>
                 </li>
             </ul>
-            <ul class="navbar-nav ms-auto">
+            <ul class="navbar-nav ms-auto m-hidden">
                 <li class="nav-item">
                     <a class="nav-link" href="#" data-bs-toggle="modal" data-bs-target="#searchModal">
                         <i class="fa fa-search"></i>
@@ -33,7 +53,6 @@
                         <i class="fa-solid fa-user"></i>
                     </a>
                 </li>
-
                 <li class="nav-item">
                     <a class="nav-link" data-bs-toggle="offcanvas" href="#offcanvasCart" role="button"
                         aria-controls="offcanvasCart">
