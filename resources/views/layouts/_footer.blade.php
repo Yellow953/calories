fe6a6432dcb713657187803d6e740b9675c659ce
EdwardBejjani <edwardbejjani@gmail.com>
2024-12-18 13:53:35 +0200

    footer link add

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -7,14 +7,16 @@
             <img src="{{ asset('assets/images/logo.png') }}" alt="" class="img-fluid img-footer rounded-white ms-md-4 mt-5 mt-md-0">
         </div>
         <div class="col-12 col-md-7 text-shadow">
-            <div class="ms-md-5 mt-5">
+            <div class="ms-md-5 mt-5 pb-4">
                 <h3 class="text-white fw-bold text-center">Get in Touch</h3>
                 <div class="row ms-0 me-0">
                     <div class="col-6">
                         <ul class="list-unstyled text-center">
-                            <li style=" font-size: 2rem;"><i class="fa-solid fa-location-dot text-white"></i></li>
-                            <li class="text-white">Somewhere for example</li>
-                            <li class="text-white">Lebanon</li>
+                            <a href="#" class="text-decoration-none">
+                                <li style=" font-size: 2rem;"><i class="fa-solid fa-location-dot text-white"></i></li>
+                                <li class="text-white">Somewhere for example</li>
+                                <li class="text-white">Lebanon</li>
+                            </a>
                         </ul>
                     </div>
                     <div class="col-6">
@@ -29,29 +31,37 @@
                 <div class="row ms-0 me-0">
                     <div class="col-6">
                         <ul class="list-unstyled text-center">
-                            <li style=" font-size: 2rem;"><i class="fa fa-phone text-white"></i></li>
-                            <li class="text-white">+961 70 123 456</li>
+                            <a href="#" class="text-decoration-none">
+                                <li style=" font-size: 2rem;"><i class="fa fa-phone text-white"></i></li>
+                                <li class="text-white">+961 70 123 456</li>
+                            </a>
                         </ul>
                     </div>
                     <div class="col-6">
                         <ul class="list-unstyled text-center">
-                            <li style=" font-size: 2rem;"><i class="fa fa-envelope text-white"></i></li>
-                            <li class="text-white">email@example.com</li>
+                            <a href="#" class="text-decoration-none">
+                                <li style=" font-size: 2rem;"><i class="fa fa-envelope text-white"></i></li>
+                                <li class="text-white">email@example.com</li>
+                            </a>
                         </ul>
                     </div>
                 </div>
                 <div class="row ms-0 me-0">
                     <div class="col-6">
                         <ul class="list-unstyled text-center">
-                            <li style=" font-size: 2rem;"><i class="fa-brands fa-instagram text-white"></i></li>
-                            <li class="text-white">Instagram</li>
+                            <a href="#" class="text-decoration-none">
+                                <li style=" font-size: 2rem;"><i class="fa-brands fa-instagram text-white"></i></li>
+                                <li class="text-white">Instagram</li>
+                            </a>
                         </ul>
                     </div>
 
                     <div class="col-6">
                         <ul class="list-unstyled text-center">
-                            <li style=" font-size: 2rem;"><i class="fa-brands fa-facebook text-white"></i></li>
-                            <li class="text-white">Facebook</li>
+                            <a href="#" class="text-decoration-none">
+                                <li style=" font-size: 2rem;"><i class="fa-brands fa-facebook text-white"></i></li>
+                                <li class="text-white">Facebook</li>
+                            </a>
                         </ul>
                     </div>
                 </div>
