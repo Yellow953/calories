65608b61649ec5db9caa59e53d5f9d705e366af7
EdwardBejjani <edwardbejjani@gmail.com>
2024-12-25 16:39:23 +0200

    newsletter section

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -29,118 +29,140 @@ $translator->setTarget(app()->getLocale());
         </div>
     </div>
 </div>
-<div class="container pb-5">
-    <section>
-        <div>
-            <h2 class="hero-title fw-bold text-center mb-4">{{__('landing.categories')}}</h2>
-            <div class="owl-carousel owl-theme categories">
-                @foreach ($categories as $category)
-                <div class="category-item bg-white">
-                    <a href="{{ route('shop') }}?category={{ urlencode($category->name) }}"
-                        class="text-decoration-none text-primary">
-                        <div class="category-image">
-                            <img src="{{ asset($category->image) }}" class="img-fluid category-img">
-                        </div>
-                        <div class="d-flex flex-column category-title">
-                            <h5 class="text-center mt-2">{{ $translate ? $translator->translate($category->name) :
+<div class="container py-5">
+    <div>
+        <h2 class="hero-title fw-bold text-center mb-4">{{__('landing.categories')}}</h2>
+        <div class="owl-carousel owl-theme categories">
+            @foreach ($categories as $category)
+            <div class="category-item bg-white">
+                <a href="{{ route('shop') }}?category={{ urlencode($category->name) }}"
+                    class="text-decoration-none text-primary">
+                    <div class="category-image">
+                        <img src="{{ asset($category->image) }}" class="img-fluid category-img">
+                    </div>
+                    <div class="d-flex flex-column category-title">
+                        <h5 class="text-center mt-2">{{ $translate ? $translator->translate($category->name) :
                                 $category->name }}</h5>
+                    </div>
+                </a>
+            </div>
+            @endforeach
+        </div>
+    </div>
+
+    <div class="row mb-5">
+        <div class="col-md-4 mb-3 mb-md-0">
+            <div class="card home-card card-1">
+                <img src="{{ asset('assets/images/organic.jpg') }}" class="card-img-top" alt="">
+                <div class="card-body">
+                    <h5 class="card-title">{{__('landing.organic_products')}}</h5>
+                    <p class="card-text">{{__('landing.msg1')}}</p>
+                    <div>
+                        <a href="{{ route('shop') }}?category={{ urlencode('Organic') }}"
+                            class="btn btn-primary">{{__('landing.shopnow')}}</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4 mb-3 mb-md-0">
+            <div class="card home-card card-2">
+                <img src="{{ asset('assets/images/organic.jpg') }}" class="card-img-top" alt="">
+                <div class="card-body">
+                    <h5 class="card-title">{{__('landing.sugar-free')}} <br>
+                        {{__('landing.healthy_snacks')}}
+                    </h5>
+                    <p class="card-text">{{__('landing.msg2')}}</p>
+                    <div>
+                        <a href="{{ route('shop') }}?category={{ urlencode('Sugar Free/Healthy Snacks') }}"
+                            class="btn btn-primary">{{__('landing.shopnow')}}</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4 mb-3 mb-md-0">
+            <div class="card home-card card-3">
+                <img src="{{ asset('assets/images/organic.jpg') }}" class="card-img-top" alt="">
+                <div class="card-body">
+                    <h5 class="card-title">{{__('landing.dried_fruits')}}</h5>
+                    <p class="card-text">{{__('landing.tasty_and_healthy')}}</p>
+                    <div>
+                        <a href="{{ route('shop') }}?category={{ urlencode('Dried Fruits') }}"
+                            class="btn btn-primary">{{__('landing.shopnow')}}</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row mb-5">
+        <div class="col-md-12 mb-3 mb-md-0">
+            <div class="card card-newsletter text-center">
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col-md-6 text-start d-flex flex-column justify-content-center">
+                            <h2 class="hero-title mb-3">Join Our Newsletter</h2>
+                            <p class="card-text mb-3">Join our e-mail subscription now to get the latest updates about upcoming promotions, discounts, and new products!</p>
+                            <form action="#" method="POST">
+                                @csrf
+                                <div class="mb-3">
+                                    <input type="email" name="email" class="form-control" placeholder="Enter Your Email" required>
+                                </div>
+                                <button type="submit" class="btn btn-primary">Subscribe</button>
+                            </form>
                         </div>
+                        <div class="col-md-6">
+                            <img src="{{asset('assets/images/email-signup.png')}}" alt="" class="img-fluid email-img">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="mt-4">
+        <h2 class="hero-title fw-bold text-center mb-4">{{__('landing.best_sellers')}}</h2>
+        <div class="owl-carousel owl-theme products">
+            @foreach ($products as $product)
+            <div class="category-item bg-white">
+                <div class="product-img">
+                    <a href="{{ route('product', $product->name) }}">
+                        <img src="{{ asset($product->image) }}" class="img-fluid category-img">
                     </a>
                 </div>
-                @endforeach
-            </div>
-        </div>
-
-        <div class="row mb-5">
-            <div class="col-md-4 mb-3 mb-md-0">
-                <div class="card home-card card-1">
-                    <img src="{{ asset('assets/images/organic.jpg') }}" class="card-img-top" alt="">
-                    <div class="card-body">
-                        <h5 class="card-title">{{__('landing.organic_products')}}</h5>
-                        <p class="card-text">{{__('landing.msg1')}}</p>
-                        <div>
-                            <a href="{{ route('shop') }}?category={{ urlencode('Organic') }}"
-                                class="btn btn-primary">{{__('landing.shopnow')}}</a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-4 mb-3 mb-md-0">
-                <div class="card home-card card-2">
-                    <img src="{{ asset('assets/images/organic.jpg') }}" class="card-img-top" alt="">
-                    <div class="card-body">
-                        <h5 class="card-title">{{__('landing.sugar-free')}} <br>
-                            {{__('landing.healthy_snacks')}}
-                        </h5>
-                        <p class="card-text">{{__('landing.msg2')}}</p>
-                        <div>
-                            <a href="{{ route('shop') }}?category={{ urlencode('Sugar Free/Healthy Snacks') }}"
-                                class="btn btn-primary">{{__('landing.shopnow')}}</a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-4 mb-3 mb-md-0">
-                <div class="card home-card card-3">
-                    <img src="{{ asset('assets/images/organic.jpg') }}" class="card-img-top" alt="">
-                    <div class="card-body">
-                        <h5 class="card-title">{{__('landing.dried_fruits')}}</h5>
-                        <p class="card-text">{{__('landing.tasty_and_healthy')}}</p>
-                        <div>
-                            <a href="{{ route('shop') }}?category={{ urlencode('Dried Fruits') }}"
-                                class="btn btn-primary">{{__('landing.shopnow')}}</a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-        <div class="mt-4">
-            <h2 class="hero-title fw-bold text-center mb-4">{{__('landing.best_sellers')}}</h2>
-            <div class="owl-carousel owl-theme products">
-                @foreach ($products as $product)
-                <div class="category-item bg-white">
-                    <div class="product-img">
-                        <a href="{{ route('product', $product->name) }}">
-                            <img src="{{ asset($product->image) }}" class="img-fluid category-img">
-                        </a>
-                    </div>
-                    <h5 class="category-title text-center text-primary mt-2">{{ $translate ?
+                <h5 class="category-title text-center text-primary mt-2">{{ $translate ?
                         $translator->translate($product->name) : $product->name }}</h5>
-                </div>
-                @endforeach
             </div>
-            </d>
+            @endforeach
         </div>
+    </div>
+    <div class="row mt-4">
 
-        <div class="row mt-4">
-            <div class="col-md-6 mb-3 mb-md-0">
-                <div class="card home-card card-4">
-                    <img src="{{ asset('assets/images/organic.jpg') }}" class="card-img-top" alt="">
-                    <div class="card-body">
-                        <h5 class="card-title">{{__('landing.weight_loss_supplements')}}</h5>
-                        <p class="card-text">{{__('landing.msg3')}}</p>
-                        <div>
-                            <a href="{{ route('shop') }}?category={{ urlencode('Weight Loss Supplements') }}"
-                                class="btn btn-primary">{{__('landing.shopnow')}}</a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-6 mb-3 mb-md-0">
-                <div class="card home-card card-5">
-                    <img src="{{ asset('assets/images/organic.jpg') }}" class="card-img-top" alt="">
-                    <div class="card-body">
-                        <h5 class="card-title">{{__('landing.protein_snacks')}}</h5>
-                        <p class="card-text">{{__('landing.msg4')}}</p>
-                        <div>
-                            <a href="{{ route('shop') }}?category={{ urlencode('Protein Snacks') }}"
-                                class="btn btn-primary">{{__('landing.shopnow')}}</a>
-                        </div>
+    </div>
+    <div class="row mt-4">
+        <div class="col-md-6 mb-3 mb-md-0">
+            <div class="card home-card card-4">
+                <img src="{{ asset('assets/images/organic.jpg') }}" class="card-img-top" alt="">
+                <div class="card-body">
+                    <h5 class="card-title">{{__('landing.weight_loss_supplements')}}</h5>
+                    <p class="card-text">{{__('landing.msg3')}}</p>
+                    <div>
+                        <a href="{{ route('shop') }}?category={{ urlencode('Weight Loss Supplements') }}"
+                            class="btn btn-primary">{{__('landing.shopnow')}}</a>
                     </div>
                 </div>
             </div>
         </div>
-    </section>
+        <div class="col-md-6 mb-3 mb-md-0">
+            <div class="card home-card card-5">
+                <img src="{{ asset('assets/images/organic.jpg') }}" class="card-img-top" alt="">
+                <div class="card-body">
+                    <h5 class="card-title">{{__('landing.protein_snacks')}}</h5>
+                    <p class="card-text">{{__('landing.msg4')}}</p>
+                    <div>
+                        <a href="{{ route('shop') }}?category={{ urlencode('Protein Snacks') }}"
+                            class="btn btn-primary">{{__('landing.shopnow')}}</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
 @endsection
